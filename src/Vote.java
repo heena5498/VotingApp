5aa5181c073625d7aca553c326cb24998173fbe9
Jay <108143448+godlyjenna@users.noreply.github.com>
2025-06-27 00:14:12 -0800

    Update Vote.java
    Changed vote attribute to candidate for broader vote variety

--- src/Vote.java
+++ src/Vote.java
@@ -1,24 +1,25 @@
 public class Vote {
 
     private final String voterId;
-    private final String choice; // Yay or Nay
+    private final String candidate; // take user input
 
-    public Vote(String voterId, String choice){
+    public Vote(String voterId, String candidate){
         this.voterId = voterId;
-        this.choice = choice;
+        this.candidate = candidate;
     }
 
     public String getVoterId(){
         return voterId;
     }
 
-    public String choice(){
-        return choice;
+    public String getCandidate(){
+        return candidate;
     }
+
 
     @Override
     public String toString(){
-        return "Voter: " + voterId + ", Vote: " + choice;
+        return "Voter: " + voterId + ", Vote: " + candidate;
     }
 
 }
